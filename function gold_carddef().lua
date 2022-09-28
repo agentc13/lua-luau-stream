2320f639ec53bd251f7ba9c1cf6f7d326a5e59da
agentc13 <timothy.a.mckenzie@gmail.com>
2022-09-28 13:26:48 -0400

    added gold_carddef() file

---
+++ function gold_carddef().lua
@@ -0,0 +1,19 @@
+---@diagnostic disable: undefined-global, lowercase-global
+
+
+function gold_carddef()
+    return createDef({
+        id = "gold",
+        name = "Gold",
+        types = { itemType, currencyType, noStealType  },
+        acquireCost = 0,
+        abilities = createAbility({
+            id = "gold_ab",
+            trigger = autoTrigger,
+            effect = gainGoldEffect(1)
+        }),
+        layout = loadLayoutTexture("Textures/gold"),
+        cardTypeLabel = "Item",
+        playLocation = castPloc,
+    })
+end
